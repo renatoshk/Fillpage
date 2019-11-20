9ed8659e8443fe66592ac5c49bf202f769f742f4
Renato Shkulaku <shkulakurenato@gmail.com>
2019-11-20 21:00:07 +0100

    commit users with photo fetaure

--- app/Http/Controllers/AdminUsersController.php
+++ app/Http/Controllers/AdminUsersController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\User;
 use App\Role;
+use App\Photo;
 use App\Http\Requests\UsersRequest;
 
 class AdminUsersController extends Controller
@@ -41,10 +42,19 @@ class AdminUsersController extends Controller
      */
     public function store(UsersRequest $request)
     {
-        //
-        User::create($request->all());
+
+        $input = $request->all();
+        if($file = $request->file('photo_id')){
+          $name = time() . $file->getClientOriginalName();
+          $file->move('images', $name);
+          $photo = Photo::create(['file'=>$name]);
+          $input['photo_id'] = $photo->id;
+
+        }
+        $input['password'] = md5($request->password);
+        User::create($input);
         return redirect('/admin/users');
-        // return $request->all();
+        
     }
 
     /**
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,7 +23,7 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a style="color: white; font-size: 30px;" class="navbar-brand" href="{{ url('/') }}">
+                <a style="color: red; font-size: 30px;" class="navbar-brand" href="{{ url('/') }}">
                     {{ config('app.name', 'Fillpage.com') }}
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
@@ -41,11 +41,11 @@
                         <!-- Authentication Links -->
                         @guest
                             <li class="nav-item">
-                                <a style="color: white; font-size: 25px;" class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                <a style="color: red; font-size: 25px;" class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
                             </li>
                             @if (Route::has('register'))
                                 <li class="nav-item">
-                                    <a style="color: white; font-size: 25px;" class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                    <a style="color: red; font-size: 25px;" class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
                                 </li>
                             @endif
                         @else
